454f0b34a0c1451205c818cdf302253341cfca88
Victor Akanbi <vakanbi971@west-mec.org>
2019-02-27 13:35:46 -0700

    Final Commit
    Ready for review

--- bc_today.js
+++ bc_today.js
@@ -1,26 +1,36 @@
-
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 2
 
    Today at the Union Script
-   Author: 
-   Date:   
+   Author: Victor Akanbi
+   Date:   2.25.19
    
    This script uses the getEvent() function to return the
    HTML code containin the daily events at the Bridger College
    student union.
 
 */
+getDay();
+setInterval('getDay()', 1000);
 
+function getDay() {
+    var thisDate = new Date();
+    var dateString = thisDate.toLocaleDateString();
+    var thisDay = thisDate.getDay();
+    var dateHTML = `<h1>Today at the Union</h1> <h2 id="dateString">${dateString}</h2>`;
+    var eventHTML = getEvent(thisDay);
+    document.getElementById('unionToday').innerHTML = dateHTML + eventHTML;
 
+}
 
 function getEvent(day) {
-   var eventHTML;
-   switch (day) {
-      case 0: // Sunday Events
-      eventHTML = "<dl> \
+    var eventHTML;
+    switch (day) {
+
+        case 0: // Sunday Events
+            eventHTML = "<dl> \
          <dt>Highlights from the Bridger Art Collection</dt> \
          <dd><p>An exhibition from over 60 items in the BC permanent collection.</p> \
              <p><strong>Location:</strong> Room A414</p> \
@@ -34,9 +44,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> $3.75 MWU students, Union members, Union staff. $4.25 all others</p> \
          </dd> \
       </dl>";
-      break;
-      case 1: // Monday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 1: // Monday Events
+            eventHTML = "<dl> \
          <dt>Monday Billiards</dt> \
          <dd><p>Play in the BC Billiards league for fun and prizes</p> \
              <p><strong>Location:</strong> Union Game Room </p> \
@@ -50,9 +61,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> free, seating is limited</p> \
          </dd> \
       </dl>";
-      break;
-      case 2: // Tuesday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 2: // Tuesday Events
+            eventHTML = "<dl> \
          <dt>Alternative Breaks Spring Break Information Session</dt> \
          <dd><p>Make your spring break matter! Alt Breaks offers students inexpensive trips \
                (all under $500) around the US to participate on service projects in places such \
@@ -70,9 +82,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> free</p> \
          </dd> \
       </dl>";
-      break;      
-         case 3: // Wednesday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 3: // Wednesday Events
+            eventHTML = "<dl> \
          <dt>Volunteer Fair</dt> \
          <dd><p>Over 20 community organizations and schools will be on hand to promote their \
                volunteer opportunities with youth programs, enviromental issues, child care, \
@@ -95,9 +108,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> free</p> \
          </dd> \
       </dl>";
-      break;
-      case 4: // Thursday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 4: // Thursday Events
+            eventHTML = "<dl> \
          <dt>Thursday Nite Open Mike</dt> \
          <dd><p>New later hours with more acts and karaoke added to the mix! \
                 Students performing for students. Get your 15 minutes of \
@@ -120,9 +134,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> free to students; $30 for guests (15 week session)</p> \
          </dd> \
       </dl>";
-      break;         
-         case 5: // Friday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 5: // Friday Events
+            eventHTML = "<dl> \
          <dt>Conversation &amp; Coffee Hour</dt> \
          <dd><p>Meet people from around the world in a relaxing atmosphere. \
                 Enjoy coffee, snacks, and informal conversation with  \
@@ -145,9 +160,10 @@ function getEvent(day) {
              <p><strong>Cost:</strong> $5.50</p> \
          </dd> \
       </dl>";
-      break;
-      case 6: // Saturday Events
-      eventHTML = "<dl> \
+            break;
+
+        case 6: // Saturday Events
+            eventHTML = "<dl> \
          <dt>Kites on the Concourse</dt> \
          <dd><p>Enjoy beautiful kites from around the world and then make and \
                 fly kites of your own invention.</p>\
@@ -169,8 +185,8 @@ function getEvent(day) {
              <p><strong>Cost:</strong> $5.75 with student ID</p> \
          </dd> \
       </dl>";
-      break;      
-   }
-   
-   return eventHTML
+            break;
+    }
+
+    return eventHTML
 }
